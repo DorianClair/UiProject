e95a4551e85e24c3f9fcb79fc8d59340bca6607f
Sam White <swhite3@unb.ca>
2019-11-14 14:51:59 -0400

    add canvas stuff to view

--- src/application/View.java
+++ src/application/View.java
@@ -1,7 +1,13 @@
 package application;
 
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+
 public class View {
 
+	Canvas canvas = new Canvas();
+	GraphicsContext gc = canvas.getGraphicsContext2D();
+	
 	public View() {
 		
 	}
