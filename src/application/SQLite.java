ab7e63168b01fa0582e1a7722f408983ae18a7eb
Griffin Higgins <griffin.higgins@unb.ca>
2019-11-30 18:06:53 -0400

    Information Entry screen updated, welcome screen added, and SQLite class modified

--- src/application/SQLite.java
+++ src/application/SQLite.java
@@ -1,62 +1,67 @@
 package application;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-
 public class SQLite {
 	private static Connection con;
-	private static boolean hasData = false;
-	
-//	public ResultSet displayUsers() throws ClassNotFoundException, SQLException {
-//		if(con == null) {
-//			getConnection();
-//		}
-//		Statement state = con.createStatement();
-//		ResultSet res = state.executeQuery("SELECT fname, lname FROM user");
-//		return res;
-//	}
-	
-	
-
+	public ResultSet displayUsers() throws ClassNotFoundException, SQLException {
+		if(con == null) {
+			getConnection();
+		}
+		Statement state = con.createStatement();
+		ResultSet res = state.executeQuery("SELECT name FROM info");
+		return res;
+	}
 	public void getConnection() throws ClassNotFoundException, SQLException {
 		Class.forName("org.sqlite.JDBC");
 		con = DriverManager.getConnection("jdbc:sqlite:database.db");
-		intitalize(); //change this
+		intitalize();
 	}
-	
-	private void intitalize() throws SQLException {
-		if(!hasData) {
-			hasData = true;
-		}
+	public void intitalize() throws SQLException {
 		Statement state = con.createStatement();
-		ResultSet res = state.executeQuery("SELECT name FROM sqlite_master WHERE type='table' AND name='user'");
+		ResultSet res = state.executeQuery("SELECT name FROM sqlite_master WHERE type='table' AND name='info'");
 		if(!res.next()) {
-			System.out.println("populate tables");
-			Statement state2 = con.createStatement();
-			state2.execute("CREATE TABLE user(id integer,"
-							+ "fname varchar(50),"
-							+ "lname varchar(50),"
-							+ "primary key(id));");
-			
-			PreparedStatement prep = con.prepareStatement("INSERT INTO user values(?,?,?)");
-			prep.setString(2, "John");
-			prep.setString(3, "Dave");
-			prep.execute();
+//			System.out.println("populateing info tables...");
+			Statement state3 = con.createStatement();
+			state3.execute("CREATE TABLE info (info_ID INT,"
+							+ "name VARCHAR(50),"
+							+ "DOB DATE,"
+							+ "Height INT,"
+							+ "Gender INT,"
+							+ "primary key(info_ID));");
+		}
+		
+		res = state.executeQuery("SELECT name FROM sqlite_master WHERE type='table' AND name='stuff'");
+		if(!res.next()) {
+//			System.out.println("populateing stuff tables...");
+			Statement state4 = con.createStatement();
+			state4.execute("CREATE TABLE stuff (stuff_ID INT,"
+							+ "date DATE,"
+							+ "budget INT,"
+							+ "consumed INT,"
+							+ "primary key(stuff_ID));");
+		}
+		res = state.executeQuery("SELECT name FROM sqlite_master WHERE type='table' AND name='weight'");
+		if(!res.next()) {
+//			System.out.println("populateing weight tables...");
+			Statement state5 = con.createStatement();
+			state5.execute("CREATE TABLE weight (weight_ID INT,"
+							+ "date DATE,"
+							+ "weight INT,"
+							+ "primary key(weight_ID));");
 		}
 	}
-	
-	
-	
-//	public void addUser(String fname, String lname) throws ClassNotFoundException, SQLException {
-//		if(con == null) {
-//			getConnection();
-//		}
-//		PreparedStatement prep = con.prepareStatement("INSERT INTO user values(?,?,?);");
-//		prep.setString(2, fname);
-//		prep.setString(3, lname);
-//		prep.execute();
-//	}
+	public void execute(String str) throws SQLException {
+		System.out.println("SQLite.java EXECUTED: " + str);
+		Statement state = con.createStatement();
+		state.execute(str);
+	}
+	public ResultSet query(String str) throws SQLException {
+		System.out.println("SQLite.java QUERIED: " + str);
+		Statement state = con.createStatement();
+		ResultSet res = state.executeQuery(str);
+		return res;
+	}
 }
